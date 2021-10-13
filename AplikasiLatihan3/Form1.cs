8e0800d99a952b2deca53647f75dd4ba0b239e7b
YusufDarmawan <yusufdarmawan@students.amikom.ac.id>
2021-10-13 16:30:55 +0700

    first commit

---
+++ AplikasiLatihan3/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AplikasiLatihan3
+{
+    public partial class frmLatihan33 : Form
+    {
+        public frmLatihan33()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            var nama = txtNama.Text;
+            var pendidikan = cmbPendidikan.Text;
+            var pekerjaan = lstPekerjaan.Text;
+
+            txtPesan1.Text = string.Format("Hallo {0}", nama);
+            txtPesan2.Text = string.Format("Pendidikan Anda {0} ya?", pendidikan);
+            txtPesan3.Text = string.Format("Anda seorang {0}, hebat !!!", pekerjaan);
+
+        }
+    }
+}
